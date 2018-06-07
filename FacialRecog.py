fc034e4aa6417919f828d67428b772e502f698d4
Michael Sebastian <mic.sebastian@hotmail.com>
2018-06-07 14:15:55 +0800

    edit FacialRecog to have multiple faces in one object

--- FacialRecog.py
+++ FacialRecog.py
@@ -6,71 +6,61 @@ from time import sleep
 
 class FacialRecog(object):
 
-    def __init__(self, name, img):
-        self.name = name
-        self.img = img
-        classifier = "haarcascade_frontalface_alt.xml"
-        self.face_cascade = cv2.CascadeClassifier(classifier)
+    def __init__(self):
+        self.name_list = ['']
+        self.faces = []
+        self.labels = []
+        classifiers = ['haarcascade_frontalface_alt.xml', 'lbpcascade_frontalface.xml']
+        self.face_cascade = cv2.CascadeClassifier(classifiers[1])
         self.face_recognizer = cv2.face.LBPHFaceRecognizer_create()
-        # dirs = os.listdir('michael-face')
-        # faces_list = []
-        # labels = []
-        # for file_name in dirs:
-        #     image_path = 'michael-face/%s' % file_name
-        #     image = cv2.imread(image_path)
-        #     cv2.imshow('Training on image...', cv2.resize(image, (400, 500)))
-        #     cv2.waitKey(100)
-        #     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        #     faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5)
-        #     (x, y, w, h) = faces[0]
-        #     faces_list.append(gray[y:y+w, x:x+h])
-        #     labels.append(1)
-        # cv2.destroyAllWindows()
-        # self.face_recognizer.train(faces_list, np.array(labels))
+
+    def add_new(self, name, img):
+        if name not in self.name_list:
+            self.name_list.append(name)
+        label = self.name_list.index(name)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5)
-        (x, y, w, h) = faces[0]
-        self.face = gray[y:y+w, x:x+h]
-        self.rect = faces[0]
-        self.face_recognizer.train([self.face], np.array([1]))
+        if len(faces) == 1:
+            (x, y, w, h) = faces[0]
+            # face = gray[x:x+w, y:y+h]
+            face = gray[y:y+w, x:x+h]
+            self.faces.append(face)
+            self.labels.append(label)
+            self.face_recognizer.train(self.faces, np.array(self.labels))
+            print(name + ' face added')
+        else:
+            print('Invalid. Please ensure that there is only one face in the picture.')
 
-    def identify_face(self, face):
-        label, confidence = self.face_recognizer.predict(face)
+    def predict(self, face, gray):
+        (x, y, w, h) = face
+        # test_face = gray[x:x+w, y:y+h]
+        test_face = gray[y:y+w, x:x+h]
+        label, confidence = self.face_recognizer.predict(test_face)
         return label, confidence
 
-    def detect_faces(self, img):
-        new_img = img.copy()
-        gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
-        faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
-        if len(faces) != 0:
-            for face in faces:
-                (x, y, w, h) = face
-                to_test = gray[x:x+w, y:y+h]
-                label, confidence = self.identify_face(to_test)
-                print(face)
-                cv2.rectangle(new_img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-                cv2.putText(new_img, str(label) + ' ' + str(confidence), (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
-        return new_img
+    def detect_faces(self, test_img):
+        img = test_img.copy()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=7)
+        if len(faces) > 0:
+            return faces, gray
+        else:
+            return None, None
 
-    def get_face(self):
-        # Check if the face of the facial_data is within view, returns a bool
-        is_there = True
-        return is_there
-
-    def get_distance(self):
-        # Get the distance between the bot and the face, returns a float
-        distance = float(1.0)
-        return distance
-
-    def get_angle(self):
-        # Get the angle of position of face, returns a float
-        angle = float(1.0)
-        return angle
+    def box_face(self, test_img, label, confidence, face):
+        img = test_img.copy()
+        (x, y, w, h) = face
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(img, self.name_list[label] + ' %.2f' % confidence, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
+        return img
 
 
 if __name__ == '__main__':
-    img = cv2.imread('1.jpg')
-    my_face = FacialRecog('Michael', img)
+    facial_recognition = FacialRecog()
+    img_michael = cv2.imread('michael.jpg')
+    img_meimei = cv2.imread('meimei.jpg')
+    facial_recognition.add_new('Michael', img_michael)
+    facial_recognition.add_new('Mei Mei', img_meimei)
     video_capture = cv2.VideoCapture(0)
     while True:
         if not video_capture.isOpened():
@@ -78,9 +68,96 @@ if __name__ == '__main__':
             sleep(5)
             continue
         ret, frame = video_capture.read()
-        new_frame = my_face.detect_faces(frame)
+        faces, gray = facial_recognition.detect_faces(frame)
+        new_frame = frame
+        if faces is not None:
+            for face in faces:
+                label, confidence = facial_recognition.predict(face, gray)
+                new_frame = facial_recognition.box_face(new_frame, label, confidence, face)
         cv2.imshow('Video', new_frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     video_capture.release()
     cv2.destroyAllWindows()
+
+
+# class FacialRecog(object):
+#
+#     def __init__(self, name, img):
+#         self.name = name
+#         self.img = img
+#         classifier = "haarcascade_frontalface_alt.xml"
+#         self.face_cascade = cv2.CascadeClassifier(classifier)
+#         self.face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+#         # dirs = os.listdir('michael-face')
+#         # faces_list = []
+#         # labels = []
+#         # for file_name in dirs:
+#         #     image_path = 'michael-face/%s' % file_name
+#         #     image = cv2.imread(image_path)
+#         #     cv2.imshow('Training on image...', cv2.resize(image, (400, 500)))
+#         #     cv2.waitKey(100)
+#         #     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#         #     faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5)
+#         #     (x, y, w, h) = faces[0]
+#         #     faces_list.append(gray[y:y+w, x:x+h])
+#         #     labels.append(1)
+#         # cv2.destroyAllWindows()
+#         # self.face_recognizer.train(faces_list, np.array(labels))
+#         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#         faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5)
+#         (x, y, w, h) = faces[0]
+#         self.face = gray[y:y+w, x:x+h]
+#         self.rect = faces[0]
+#         self.face_recognizer.train([self.face], np.array([1]))
+#
+#     def identify_face(self, face):
+#         label, confidence = self.face_recognizer.predict(face)
+#         return label, confidence
+#
+#     def detect_faces(self, img):
+#         new_img = img.copy()
+#         gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+#         faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
+#         if len(faces) != 0:
+#             for face in faces:
+#                 (x, y, w, h) = face
+#                 to_test = gray[x:x+w, y:y+h]
+#                 label, confidence = self.identify_face(to_test)
+#                 print(face)
+#                 cv2.rectangle(new_img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+#                 cv2.putText(new_img, str(label) + ' ' + str(confidence), (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
+#         return new_img
+#
+#     def get_face(self):
+#         # Check if the face of the facial_data is within view, returns a bool
+#         is_there = True
+#         return is_there
+#
+#     def get_distance(self):
+#         # Get the distance between the bot and the face, returns a float
+#         distance = float(1.0)
+#         return distance
+#
+#     def get_angle(self):
+#         # Get the angle of position of face, returns a float
+#         angle = float(1.0)
+#         return angle
+#
+#
+# if __name__ == '__main__':
+#     img = cv2.imread('1.jpg')
+#     my_face = FacialRecog('Michael', img)
+#     video_capture = cv2.VideoCapture(0)
+#     while True:
+#         if not video_capture.isOpened():
+#             print('Unable to load camera')
+#             sleep(5)
+#             continue
+#         ret, frame = video_capture.read()
+#         new_frame = my_face.detect_faces(frame)
+#         cv2.imshow('Video', new_frame)
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
+#     video_capture.release()
+#     cv2.destroyAllWindows()
